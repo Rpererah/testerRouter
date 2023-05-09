27891ae4c015aceee733410da187dfbf875861d1
Rafael <rpererah@gmail.com>
2023-05-09 17:08:41 -0300

    aula02

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import Menulink from './Menulink'
-
+import React from 'react';
+import Menulink from './Menulink';
+import estilo from './Menu.module.css';
 export default function Menu() {
   return (
-    <div>
+    <div className={estilo.container}>
       <Menulink to={'/'}> Home </Menulink>   
       <Menulink to={'sobrenos'}> Sobrenos </Menulink>
     </div>
--- src/components/Menu/Menulink.js
+++ src/components/Menu/Menulink.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Menulink({children,to}) {
+  const localizacao=useLocation().pathname;
   return (
     <Link to={to}>{children}</Link>
   )
